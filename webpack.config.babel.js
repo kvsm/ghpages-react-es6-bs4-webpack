38290a0031251b35915a4fc8d2231c8ba4fbe1a5
Kevin Smith <kvsm81@gmail.com>
2016-12-02 21:09:33 +0000

    Added webpack-validator

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
-import Webpack from 'webpack'
-export default {
+import validate from 'webpack-validator'
+
+const config = {
   entry: './src/App.jsx',
   output: {
     path: 'docs/assets/js/',
@@ -12,31 +13,16 @@ export default {
         test: [/\.js$/, /\.jsx$/],
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: [ 'style', 'css', 'sass' ]
       }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
-  },
-  plugins: [
-    new Webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Tether: "tether",
-      "window.Tether": "tether",
-      Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
-      Alert: "exports?Alert!bootstrap/js/dist/alert",
-      Button: "exports?Button!bootstrap/js/dist/button",
-      Carousel: "exports?Carousel!bootstrap/js/dist/carousel",
-      Collapse: "exports?Collapse!bootstrap/js/dist/collapse",
-      Dropdown: "exports?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports?Modal!bootstrap/js/dist/modal",
-      Popover: "exports?Popover!bootstrap/js/dist/popover",
-      Scrollspy: "exports?Scrollspy!bootstrap/js/dist/scrollspy",
-      Tab: "exports?Tab!bootstrap/js/dist/tab",
-      Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
-      Util: "exports?Util!bootstrap/js/dist/util",
-    })
-  ]
-};
+  }
+}
+
+export default validate(config)
